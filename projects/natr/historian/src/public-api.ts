aae69feca387873197fd8ea664087dd5b5c2f0d3
Jacob Boswell <jacob@s56.net>
2020-03-03 18:24:49 -0700

    fixed missing export

--- projects/natr/historian/src/public-api.ts
+++ projects/natr/historian/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/historian.service';
 export * from './lib/historian.module';
 export * from './lib/enums/log-level.enum';
 export * from './lib/decorators/logging.decorator';
+export * from './lib/current-log-level';
